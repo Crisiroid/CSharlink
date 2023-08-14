00841ba9bd737f12e992442068499cfb3b766a06
Crisiroid <csiroid@gmail.com>
2023-08-14 20:43:23 +0430

    Register and Hashing algorithm added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,9 +13,9 @@ namespace Csharlink.Controllers
         public CsharlinkDB db = new CsharlinkDB();
         public ActionResult Index()
         {
-            if (TempData["LoginFailed"].Equals("true"))
+            if (TempData["LoginStatus"]!= null)
             {
-                ViewBag.pm = "We don't have you on record. please register";
+                ViewBag.pm = TempData["LoginStatus"].ToString();
             }
             return View();
         }
@@ -28,20 +28,39 @@ namespace Csharlink.Controllers
         }
         public ActionResult Login(string Username, string Password)
         {
-            if (CheckUsername(Username, Password))
+            if (CheckUsername(Username, HashUtility.HashPassword(Password)))
             {
-                User user = db.Users.Find(Username);
+                User user = db.Users.FirstOrDefault(x => x.Username == Username);
                 TempData["Username"] = user.Username;
+                user.Status = "Online!";
+                db.SaveChanges();
                 Session["UserStatus"] = "Login";
                 return RedirectToAction("Index", "UserPanel");
             }
             else
             {
-                TempData["LoginFailed"] = "true";
+                TempData["LoginStatus"] = "Login Failed. Sign up!";
                 return RedirectToAction("Index", "Home");
             }
         }
 
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(User user)
+        {
+            user.Password = HashUtility.HashPassword(user.Password);
+            user.PostNum = 0;
+            user.Status = "Offline";
+            user.AccessType = "User";
+            user.Likes = 0;
+            db.Users.Add(user); 
+            db.SaveChanges();
+            TempData["LoginStatus"] = "Account Created Successfully. You can login now!";
+            return RedirectToAction("Index", "Home");
+        }
 
         public bool CheckUsername(string Username, string Password)
         {
