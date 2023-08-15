3b9aa7ad71ee9b34af69d2d8287a3fc7c69927d5
Crisiroid <csiroid@gmail.com>
2023-08-15 20:10:38 +0430

    Security improved

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,19 +26,6 @@ namespace Csharlink.Controllers
             return View();
 
         }
-        public ActionResult Login(string Username, string Password)
-        {
-            if (CheckUsername(Username, HashUtility.HashPassword(Password)))
-            {
-                Session["UserStatus"] = "Login";
-                return RedirectToAction("Index", "UserPanel", new {Username = Username});
-            }
-            else
-            {
-                TempData["LoginStatus"] = "Login Failed. Sign up!";
-                return RedirectToAction("Index", "Home");
-            }
-        }
 
         public ActionResult Register()
         {
@@ -56,11 +43,6 @@ namespace Csharlink.Controllers
             db.SaveChanges();
             TempData["LoginStatus"] = "Account Created Successfully. You can login now!";
             return RedirectToAction("Index", "Home");
-        }
-
-        public bool CheckUsername(string Username, string Password)
-        {
-            return db.Users.Any(u => u.Username == Username && u.Password == Password);
         }
 
 
--- Controllers/UserPanelController.cs
+++ Controllers/UserPanelController.cs
@@ -12,13 +12,29 @@ namespace Csharlink.Controllers
     {
         public static CsharlinkDB db = new CsharlinkDB();
 
-        public ActionResult Index(string Username)
+        [HttpPost]
+        public ActionResult Index(string Username, string Password)
         {
-            User user = db.Users.FirstOrDefault(x => x.Username == Username);
-            TempData["Username"] = user.Username;
-            user.Status = "Online!";
-            db.SaveChanges();
-            return View(user);
+            if (CheckUsername(Username, HashUtility.HashPassword(Password)))
+            {
+                User user = db.Users.FirstOrDefault(x => x.Username == Username);
+                TempData["Username"] = user.Username;
+                user.Status = "Online!";
+                Session["UserStatus"] = "Login";
+                db.SaveChanges();
+                return View(user);
+            }
+            else
+            {
+                TempData["LoginStatus"] = "Login Failed. Sign up!";
+                return RedirectToAction("Index", "Home");
+            }
+            
+        }
+
+        public bool CheckUsername(string Username, string Password)
+        {
+            return db.Users.Any(u => u.Username == Username && u.Password == Password);
         }
     }
 }
