42d3d3462d66e8367198283cc79c86ad01048d68
Crisiroid <csiroid@gmail.com>
2023-08-21 22:39:56 +0430

    Database structure update

---
+++ Models/CommentRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Csharlink.Models
+{
+    public class CommentRepository
+    {
+        public static void AddComment(Comment comment)
+        {
+            comment.CreationDate = DateTime.Now;
+            UserRepository.db.Comments.Add(comment);
+            UserRepository.db.SaveChanges();
+        }
+        public static List<Comment> GetAllComments(int postID)
+        {
+            return UserRepository.db.Comments.Where(x => x.PostID == postID).ToList();
+        }
+    }
+}
